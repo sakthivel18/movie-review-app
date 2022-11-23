3b9c3efc86495b5ad2730e2a8b82507465c26eaa
Sakthivel Ravichandran <sakthivelravichandran18@gmail.com>
2022-11-23 17:30:47 -0500

    refactored code

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/signup" element={<Signup/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h1>This is home component</h1>
+    )
+}
+
+export default Home;
